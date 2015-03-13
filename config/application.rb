995b6b7ad005e3a42903e402be87cd08cbfa6859
Navroop Singh <roopsingh93@gmail.com>
2015-03-13 11:39:39 -0700

    initial setup for devise completed

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module HepBProject
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Devise setup for deployment on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ HepBProject::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Sets default URL options for devise gem
+  config.action_mailer.default_url_options = { host: 'https://hepbproject.herokuapp.com', port: 3000 }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
