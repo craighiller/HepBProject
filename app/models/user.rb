3397a7e18cdf1d83df1e1d93f4c449ef0a86fb24
ShannonAxelrod <shanaxel42@berkeley.edu>
2015-04-01 19:28:46 -0700

    weird redirect root

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :encrypted_password
   # attr_accessible :title, :body
 end
